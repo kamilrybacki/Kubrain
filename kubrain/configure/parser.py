e3ca13a0d190dc4f1d426a044e0dd25e9258ec17
Kamil Rybacki <kamilandrzejrybacki@gmail.com>
2024-06-24 12:52:07 +0200

    Update configuration parser to remove validation and delegate schema parsing to ConfJurer

--- kubrain/configure/parser.py
+++ kubrain/configure/parser.py
@@ -1,16 +1,8 @@
 import yaml
-import kubrain.configure.validation
 
 
-def load_bytes(raw_data: bytes, data_schema: str) -> kubrain.configure.validation.ValidationResults:
+def load_bytes(raw_data: bytes) -> bytes:
     try:
-        loaded_yaml = yaml.safe_load(raw_data)
-        return kubrain.configure.validation.against_schema(
-            data=loaded_yaml, schema=data_schema
-        )
+        return yaml.safe_load(raw_data)
     except yaml.YAMLError as yaml_error:
         raise ValueError(f'Error parsing file: {yaml_error}') from yaml_error
-
-
-def load_kubrain_config(config_data: bytes) -> kubrain.configure.validation.ValidationResults:
-    return load_bytes(config_data, 'KUBRAIN')
